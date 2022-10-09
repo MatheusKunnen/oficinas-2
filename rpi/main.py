53950adff22e55e6154dad0e0fabdff329944162
Hyodar <franco.b.gomes@gmail.com>
2022-10-09 17:51:15 -0300

    Inicializa LockManager durante o setup

--- rpi/main.py
+++ rpi/main.py
@@ -1,10 +1,15 @@
 import RPi.GPIO as gpio
 import time
 
+from utils.classes.lock_manager import LockManager
+
+lock_manager = LockManager()
 
 def setup():
     gpio.setmode(gpio.BCM)
     time.sleep(SETUP_DELAY)
+
+    lock_manager.setup()
 
     print("Setup")
 
