591c91ac697fdff022844453f223e900cf72fab8
Matheus Kunnen Ledesma <matheuskl@accionproactiva.com>
2022-10-14 15:28:11 -0300

    Correção de nomes de variáveis

--- rpi/utils/classes/camera_manager.py
+++ rpi/utils/classes/camera_manager.py
@@ -9,22 +9,23 @@ from utils.constants import WEBCAM_DEVICE_ID
 class CameraManager:
     def __init__(
         self,
-        dlib_face_predictor_path=DLIB_PREDICTOR_PATH,
+        dlib_face_predictor_path=DLIB_FACE_PREDICTOR_PATH,
         dlib_face_recognizer_path=DLIB_FACE_RECOGNIZER_PATH,
         webcam_device_id=WEBCAM_DEVICE_ID,
     ):
-        self.dlib_predictor_path = dlib_predictor_path
+        self.dlib_predictor_path = dlib_face_predictor_path
         self.dlib_face_recognizer_path = dlib_face_recognizer_path
         self.webcam_device_id = webcam_device_id
 
         self.detector = dlib.get_frontal_face_detector()
         self.predictor = dlib.shape_predictor(self.dlib_predictor_path)
-        self.facerec = dlib.face_recognizer_model_v1(self.dlib_face_recognizer_path)
+        self.facerec = dlib.face_recognizer_model_v1(
+            self.dlib_face_recognizer_path)
         self.capture = cv2.VideoCapture(self.webcam_device_id)
 
     def detect_face(self):
         _, image = self.capture.read()
-        gray = cv2.cvtColor(image, cv2, COLOR_BGR2GRAY)
+        gray = cv2.cvtColor(image, cv2, cv2.COLOR_BGR2GRAY)
 
         detections = self.detector(gray, 1)
 
