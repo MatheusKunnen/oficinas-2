d27ad4de60742bb1a3ff1900910e032fa594b8f8
Franco Barpp Gomes <franco.b.gomes@gmail.com>
2022-11-12 14:41:42 -0300

    Atualiza main com máquina de estados

--- rpi/main.py
+++ rpi/main.py
@@ -2,13 +2,36 @@ import time
 
 try:
     import RPi.GPIO as gpio
+    from utils.classes.display_manager import DisplayManager
+    from utils.classes.camera_manager import CameraManager
 except:
     import Mock.GPIO as gpio
+    from mocks.classes.display_manager import DisplayManager
+    from mocks.classes.camera_manager import CameraManager
+    from mocks.classes.vault_manager import VaultManager
 
 from utils.constants import SETUP_DELAY
 from utils.classes.lock_manager import LockManager
+from utils.classes.button_manager import ButtonManager
+from utils.classes.context import Context
+from utils.classes.state_machine import StateMachine
+
+from utils.classes.states import starting_state, recognition_state, opening_state
 
 lock_manager = LockManager()
+display_manager = DisplayManager()
+button_manager = ButtonManager()
+camera_manager = CameraManager()
+vault_manager = VaultManager()
+
+state_machine = StateMachine(
+    Context(
+        lock_manager, display_manager, button_manager, camera_manager, vault_manager
+    )
+)
+state_machine.add_state(starting_state)
+state_machine.add_state(recognition_state)
+state_machine.add_state(opening_state)
 
 
 def setup():
@@ -17,9 +40,12 @@ def setup():
 
     lock_manager.setup()
     button_manager.setup()
+    vault_manager.setup()
+    state_machine.goto("starting")
 
 
 def loop():
+    state_machine.run()
     pass
 
 
@@ -29,4 +55,5 @@ def main():
         loop()
 
 
-main()
+if __name__ == "__main__":
+    main()
