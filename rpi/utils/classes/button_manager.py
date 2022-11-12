d47a4018ae48aba8cf559dc611392e9cd1c31866
Franco Barpp Gomes <franco.b.gomes@gmail.com>
2022-11-12 15:08:30 -0300

    Usar resistor de pull-up nos pinos dos botões

--- rpi/utils/classes/button_manager.py
+++ rpi/utils/classes/button_manager.py
@@ -7,19 +7,19 @@ from utils.pin import Pin
 
 
 class ButtonManager:
-    def __init__(self, channels=[Pin.BUTTON_0, Pin.BUTTON_1], bouncetime=200):
+    def __init__(self, channels=[Pin.BUTTON_0, Pin.BUTTON_1], bouncetime=500):
         self.channels = channels
         self.bouncetime = bouncetime
 
     def setup(self):
         for channel in self.channels:
-            gpio.setup(channel, gpio.IN, pull_up_down=gpio.PUD_DOWN)
+            gpio.setup(channel, gpio.IN, pull_up_down=gpio.PUD_UP)
 
     def set_callback(self, button_id, callback):
         assert button_id < len(self.channels)
 
         gpio.add_event_detect(
-            self.channels[button_id], gpio.RISING, callback, self.bouncetime
+            self.channels[button_id], gpio.FALLING, callback, self.bouncetime
         )
 
     def reset(self):
