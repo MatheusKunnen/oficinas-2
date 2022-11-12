7af8813e33d849856a87713d644fcef675881d20
Franco Barpp Gomes <franco.b.gomes@gmail.com>
2022-11-12 12:11:31 -0300

    Adiciona button_manager em Context

--- rpi/utils/classes/context.py
+++ rpi/utils/classes/context.py
@@ -6,9 +6,11 @@ except:
     from mocks.classes.display_manager import DisplayManager
 
 from utils.classes.lock_manager import LockManager
+from utils.classes.button_manager import ButtonManager
 
 
 @dataclass
 class Context:
     lock_manager: LockManager
     display_manager: DisplayManager
+    button_manager: ButtonManager
