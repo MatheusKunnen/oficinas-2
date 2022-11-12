a60521b3a47a9465a26a51a18c32e9bf93524418
Franco Barpp Gomes <franco.b.gomes@gmail.com>
2022-11-12 12:08:25 -0300

    Adiciona ButtonManager e atribui pinos em Pin

---
+++ rpi/utils/classes/button_manager.py
@@ -0,0 +1,27 @@
+try:
+    import RPi.GPIO as gpio
+except:
+    import Mock.GPIO as gpio
+
+from utils.pin import Pin
+
+
+class ButtonManager:
+    def __init__(self, channels=[Pin.BUTTON_0, Pin.BUTTON_1], bouncetime=200):
+        self.channels = channels
+        self.bouncetime = bouncetime
+
+    def setup(self):
+        for channel in channels:
+            gpio.setup(channel, gpio.IN, pull_up_down=gpio.PUD_DOWN)
+
+    def set_callback(self, button_id, callback):
+        assert button_id < len(self.channels)
+
+        gpio.add_event_detect(
+            self.channels[button_id], gpio.RISING, callback, bouncetime
+        )
+
+    def reset(self):
+        for channel in channels:
+            gpio.remove_event_detect(channel)
