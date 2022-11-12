a62d50e20cf5df40dc34b803a110534d4fa21d0e
Franco Barpp Gomes <franco.b.gomes@gmail.com>
2022-11-12 12:40:15 -0300

    Adiciona CameraManager e face em Context

--- rpi/utils/classes/context.py
+++ rpi/utils/classes/context.py
@@ -1,9 +1,13 @@
 from dataclasses import dataclass
 
+import dlib
+
 try:
     from utils.classes.display_manager import DisplayManager
+    from utils.classes.camera_manager import CameraManager
 except:
     from mocks.classes.display_manager import DisplayManager
+    from mocks.classes.camera_manager import CameraManager
 
 from utils.classes.lock_manager import LockManager
 from utils.classes.button_manager import ButtonManager
@@ -14,3 +18,5 @@ class Context:
     lock_manager: LockManager
     display_manager: DisplayManager
     button_manager: ButtonManager
+    camera_manager: CameraManager
+    face: dlib.vector = None
