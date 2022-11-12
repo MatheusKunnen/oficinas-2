2f1f9fba8768912a50f8d3ab3a17304cd5229e54
Franco Barpp Gomes <franco.b.gomes@gmail.com>
2022-11-12 14:32:22 -0300

    Corrige referência global

--- rpi/utils/classes/states/starting_state.py
+++ rpi/utils/classes/states/starting_state.py
@@ -5,8 +5,15 @@ starting_state = StateMachine.State("starting")
 should_continue = False
 
 
+def set_should_continue():
+    global should_continue
+    should_continue = True
+
+
 @starting_state.main()
 def starting(ctx, goto):
+    global should_continue
+
     if should_continue == True:
         goto("recognition")
 
@@ -15,14 +22,13 @@ def starting(ctx, goto):
 def starting_before_enter(ctx):
     ctx.display_manager.write("Aperte um botão para continuar")
 
-    def set_should_continue():
-        should_continue = True
-
     ctx.button_manager.set_callback(0, set_should_continue)
     ctx.button_manager.set_callback(1, set_should_continue)
 
 
 @starting_state.before_exit()
 def starting_before_exit(ctx):
+    global should_continue
+
     ctx.button_manager.reset()
     should_continue = False
