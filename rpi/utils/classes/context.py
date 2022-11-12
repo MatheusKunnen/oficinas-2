48555d0ca355cc19346cb9217e45127eec13a31f
Franco Barpp Gomes <franco.b.gomes@gmail.com>
2022-11-12 14:31:33 -0300

    Adiciona vault_manager em Context

--- rpi/utils/classes/context.py
+++ rpi/utils/classes/context.py
@@ -8,6 +8,7 @@ try:
 except:
     from mocks.classes.display_manager import DisplayManager
     from mocks.classes.camera_manager import CameraManager
+    from mocks.classes.vault_manager import VaultManager
 
 from utils.classes.lock_manager import LockManager
 from utils.classes.button_manager import ButtonManager
@@ -19,4 +20,5 @@ class Context:
     display_manager: DisplayManager
     button_manager: ButtonManager
     camera_manager: CameraManager
+    vault_manager: VaultManager
     face: dlib.vector = None
