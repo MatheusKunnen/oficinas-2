a7ec588b997b7ada80e9cf1ce380e4c1c41e6e50
Matheus Kunnen Ledesma <matheuskl@accionproactiva.com>
2022-11-13 00:10:10 -0300

    Transactions in database connection

--- api/src/database/DatabaseConnection.ts
+++ api/src/database/DatabaseConnection.ts
@@ -3,9 +3,28 @@ import DatabaseResult from './DatabaseResult';
 
 export default class DatabaseConnection {
   private conn: PoolClient;
+  private inTransaction = false;
 
   constructor(conn: PoolClient) {
     this.conn = conn;
+  }
+
+  public async startTransaction(): Promise<void> {
+    this.conn.query('BEGIN');
+    this.inTransaction = true;
+    return;
+  }
+
+  public async commit(): Promise<void> {
+    this.conn.query('COMMIT');
+    this.inTransaction = false;
+    return;
+  }
+
+  public async rollback(): Promise<void> {
+    this.conn.query('ROLLBACK');
+    this.inTransaction = false;
+    return;
   }
 
   public async query(queryConf: QueryConfig): Promise<DatabaseResult> {
@@ -14,6 +33,12 @@ export default class DatabaseConnection {
   }
 
   public release() {
+    if (this.inTransaction) {
+      console.error(
+        'WARNING::DatabaseConnection::Connection released with a transaction in progress.'
+      );
+      this.rollback();
+    }
     this.conn.release();
   }
 }
